f1eae14c6b2fb813b82d67a8258922e933082d60
a.murzagalin <azamat.murzagalin@deliveryhero.com>
2022-08-03 19:35:03 +0200

    uncomment ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -8,9 +8,9 @@ version = "1.0"
 
 kotlin {
     android()
-    /*iosX64()
+    iosX64()
     iosArm64()
-    iosSimulatorArm64()*/
+    //iosSimulatorArm64()
 
     /*cocoapods {
         summary = "Some description for the Shared Module"
@@ -35,24 +35,24 @@ kotlin {
         }
         val androidMain by getting
         val androidTest by getting
-        /*val iosX64Main by getting
+        val iosX64Main by getting
         val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
+        //val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+            //iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
+        //val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }*/
+            //iosSimulatorArm64Test.dependsOn(this)
+        }
     }
 }
 
